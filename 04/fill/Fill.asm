cda58f98cb7458e848e5f49e3511b1fe324993d4
hareku <hareku908@gmail.com>
2018-09-01 16:44:40 +0900

    04: fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,51 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    @24576 // read keyboard
+    D=M
+    @WHITE
+    D;JEQ // if no input => set white
+(BLACK)
+    @0
+    D=A-1
+    @color
+    M=D // all of 16 bits are 1 (i.e. -1 in decimal)
+    @FILL
+    0;JMP
+(WHITE)
+    @color
+    M=0
+(FILL)
+    @SCREEN // base address: 16384
+    D=A
+    @addr
+    M=D
+    @8192 // 32 (words/row) * 256 (row) = 8192 words in total
+    D=A
+    @n
+    M=D
+(FILL_LOOP)
+    @color
+    D=M
+    @addr
+    A=M
+    M=D
+    @addr
+    M=M+1
+    @SCREEN // fill all words => break
+    D=A
+    @addr
+    D=M-D
+    @n
+    D=M-D
+    @FILL_LOOP
+    D;JGT
+(END)
+    @LOOP
+    0;JMP
